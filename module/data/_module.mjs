6c04888e1303c8711c3e30a1d582b05e037474a7
Francis Lachance <flcfornet@fastmail.com>
2025-03-07 19:57:28 -0500

    Initial commit based on Foundry VTT boilerplate

---
+++ module/data/_module.mjs
@@ -0,0 +1,10 @@
+// Export Actors
+export {default as MistEngineActorBase} from "./base-actor.mjs";
+export {default as MistEngineCharacter} from "./actor-character.mjs";
+export {default as MistEngineNPC} from "./actor-npc.mjs";
+
+// Export Items
+export {default as MistEngineItemBase} from "./base-item.mjs";
+export {default as MistEngineItem} from "./item-item.mjs";
+export {default as MistEngineFeature} from "./item-feature.mjs";
+export {default as MistEngineSpell} from "./item-spell.mjs";
